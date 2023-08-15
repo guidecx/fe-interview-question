11c3c1be82ae8a5007f19d8e04c4dfe365cf5155
Wesley Braithwaite <wbraithwaite@me.com>
2023-08-15 16:42:29 -0600

    added a few more test placeholders

--- src/app/dev-challenge/components/UserForm/UserForm.test.tsx
+++ src/app/dev-challenge/components/UserForm/UserForm.test.tsx
@@ -8,4 +8,20 @@ describe("UserForm", () => {
       render(<UserForm />);
     }).not.toThrow();
   });
+
+  //Maybe we give the dev test plan and they can run with it?
+
+  it("displays the name of the user", () => {
+    throw new Error("not implemented");
+  });
+  
+  //These would be built-in already that validate some behaviors
+
+  it("Makes the proper call to the api to request the initial user data", () => {
+    throw new Error("not implemented");
+  });
+
+  it("Makes the proper call to the api to update the user data", () => {
+    throw new Error("not implemented");
+  });
 });
